c907a8394b37322b8caa644c9bebcffd790a8709
giladd <giladd@trainologic.com>
2017-07-31 09:26:46 +0300

    support ,ore data label options

--- src/index.js
+++ src/index.js
@@ -1,36 +1,88 @@
 import fetchJsonp from 'fetch-jsonp';
 
-function getSpreadsheet(id, sheetNum, isDoubleLabeled = false, withUpdated = true) {
-  const url = `https://spreadsheets.google.com/feeds/list/${id}/${sheetNum}/public/values?alt=json-in-script`;  
+export function raw(id, sheetNum) {
+  const url = buildUrl(id, sheetNum, 'cells');
   return new Promise((resolve, reject) => {
     fetchJsonp(url).then(function(response) {
       return response.json()
     }).then(function(json) {
-      if (isDoubleLabeled) {
-        const res = parseLabeledRowsCols(json.feed.entry, withUpdated);
-        resolve(res);
-      } else {
-        const res = parseLabeledCols(json.feed.entry, withUpdated);
-        resolve(res);
+      const data = []
+      json.feed.entry.forEach(cell => {
+        console.log(cell);
+        const row = parseInt(cell.gs$cell.row) - 1;
+        const col = parseInt(cell.gs$cell.col) - 1;
+        const content = cell.gs$cell.$t;
+        if (data[row] === undefined) {
+          data[row] = [];
+        }
+        data[row][col] = content;
+      })
+      const res = {
+        title: json.feed.title.$t,
+        updated: json.feed.updated.$t,
+        data
       }
+      resolve(res);
     }).catch(function(ex) {
       reject(ex)
     })
   })
 }
 
+export function labeledCols(id, sheetNum) {
+  const url = buildUrl(id, sheetNum, 'list');
+  return new Promise((resolve, reject) => {
+    fetchJsonp(url).then(function(response) {
+      return response.json()
+    }).then(function(json) {
+      const data = parseLabeledCols(json.feed.entry);
+      const res = {
+        title: json.feed.title.$t,
+        updated: json.feed.updated.$t,
+        data
+      }
+      resolve(res);
+    }).catch(function(ex) {
+      reject(ex)
+    })
+  })
+}
+
+export function labeledColsRows(id, sheetNum) {
+  const url = buildUrl(id, sheetNum, 'list');
+  return new Promise((resolve, reject) => {
+    fetchJsonp(url).then(function(response) {
+      return response.json()
+    }).then(function(json) {
+      const data = parseLabeledRowsCols(json.feed.entry);
+      const res = {
+        title: json.feed.title.$t,
+        updated: json.feed.updated.$t,
+        data
+      }
+      resolve(res);
+    }).catch(function(ex) {
+      reject(ex)
+    })
+  })
+}
+
+function buildUrl(id, sheetNum, mode) {
+  return `https://spreadsheets.google.com/feeds/${mode}/${id}/${sheetNum}/public/values?alt=json-in-script`;  
+}
+
 /**
  * Parser for table where just the columns are labeled
  * @return array of objects where the labels are keys
  */
-function parseLabeledCols(entries, withUpdated) {
-  return entries.map(entry => parseEntry(entry, withUpdated));
+function parseLabeledCols(entries) {
+  return entries.map(entry => parseEntry(entry));
 }
   
 /**
  * Use for table with labels only for columns
  */
-function parseEntry(entry, withUpdated) {
+function parseEntry(entry) {
   const res = {};
   Object.keys(entry).forEach(key => {
     if (key.indexOf('gsx$') === 0) {
@@ -38,9 +90,6 @@ function parseEntry(entry, withUpdated) {
       res[label] = entry[key].$t;
     }
   });
-  if (withUpdated) {
-    res._updated = entry.updated.$t;
-  }
   return res;
 }
 
@@ -48,13 +97,10 @@ function parseEntry(entry, withUpdated) {
  * Parser for table where both rows and columns are labeled
  * @return object where keys are row labels and values are objects where keys are the column labels
  */
-function parseLabeledRowsCols(entries, withUpdated) {
+function parseLabeledRowsCols(entries) {
   const res = {};
   entries.forEach(entry => {
     res[entry.title.$t] = parseLabeledRow(entry.content.$t)
-    if (withUpdated) {
-      res[entry.title.$t]._updated = entry.updated.$t;
-    }
   })
   return res;
 }
@@ -73,4 +119,8 @@ function parseLabeledRow(row) {
   return res;
 }
 
-export default getSpreadsheet;
+export default {
+  raw,
+  labeledCols,
+  labeledColsRows
+};
